4bd6fe3365a709f32a5444f22f81f9e35e4e9e75
Peterson A. Sena <petersonsena24@gmail.com>
2023-06-28 10:29:03 +0200

    adding fields to the games table

--- database/migrations/2023_06_28_082733_create_games_table.php
+++ database/migrations/2023_06_28_082733_create_games_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('home_team');
+            $table->unsignedBigInteger('away_team');
+            $table->date('game_date');
+            $table->time('game_time');
+            $table->string('arena');
+            $table->integer('score_home');
+            $table->integer('away_home');
+            $table->enum('game_status',['win', 'lose', 'comin', 'draw']);
             $table->timestamps();
+            $table->foreign('home_team')->references('id')->on('teams')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('away_team')->references('id')->on('teams')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 

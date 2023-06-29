12167e6dad1b356ed0e31e077c34460ccf176035
Peterson A. Sena <petersonsena24@gmail.com>
2023-06-29 22:45:11 +0200

    establishing relationship with game table and inverse relationship with user table

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     use HasFactory;
+    
+    protected $guarded = [];
+    //relaccion inversa con model User
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+    //relacion uno a muchos con Modelo Game
+    public function games(){
+        return $this->hasMany(Game::class, 'home_team');
+    }
+    public function oponent(){
+        return $this->hasMany(Game::class, 'away_team');
+    }
 }

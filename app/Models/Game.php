71e861925f033c76fafa729dcc8f95b143163502
Peterson A. Sena <petersonsena24@gmail.com>
2023-06-29 22:47:07 +0200

    establishing inverse relationship with the team table

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+//relacion inversa con modelo Team
+    public function localTeam(){
+        return $this->belongsTo(Team::class, 'home_team');
+    }
+    //relacion inversa con modelo Game
+    public function visitorGames() {
+        return $this->belongsTo(Game::class,'away_team');
+    }
+
 }

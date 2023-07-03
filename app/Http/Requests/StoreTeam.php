a893c0b5d8f0d2f04d539cde58ca1329021a0903
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-03 23:44:26 +0200

    creating Request class, for form validation[201~

---
+++ app/Http/Requests/StoreTeam.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTeam extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+        'name' => 'required|min:5',
+        'coach'=>'required|min:5',
+        'arena'=>'required|min:5',
+        'players'=>'required, max:55',
+        'establihed_year'=>'required'
+        ];
+    }
+    public function attributes(){
+        return['name' =>'Nombre de equipo'];
+    }
+}

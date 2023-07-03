6636034cb9eaacb575e7941ff95f452f9c061437
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-04 01:07:24 +0200

    Creating store, update and destroy methods~

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreGame;
 use App\Models\Game;
 use App\Models\Team;
 
@@ -30,14 +32,27 @@ class GameController extends Controller
 
     }
 
-    public function edit(){
+    public function edit(Game $game){
+        $teams = Team::all();	
+        return view('games.edit', compact('game', 'teams'));
 
     }
-    public function store(){
+    public function store(StoreGame $request ){
+        $game = Game::create($request->all());
+
+        return redirect()->route('games.index');
 
     }
 
-    public function update(){
+    public function update(Request $request, Game $game){
+        $game->update($request->all());
 
+        return redirect()->route('games.show', $game);
+
+
+    }
+    public function destroy(Game $game){
+        $game->delete();
+        return redirect()->route('games.index');
     }
 }

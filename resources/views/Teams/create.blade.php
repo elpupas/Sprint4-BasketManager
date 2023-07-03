af4c9c38ccdb4bcf362eb6669713808b6dec2ea8
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-03 11:50:34 +0200

    add form

--- resources/views/Teams/create.blade.php
+++ resources/views/Teams/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.plantilla')
 @section('title', 'Create')
 @section('content')
-<h1>crear</h1>
+<h1>Crear equipos</h1>
 @endsection
 <!DOCTYPE html>
 <html lang="en">
@@ -11,6 +11,25 @@
     <title>Document</title>
 </head>
 <body>
+ 
+    <form action="" method="post">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="name" required><br><br>
+    
+        <label for="coach">Coach:</label>
+        <input type="text" id="coach" name="coach" required><br><br>
+    
+        <label for="arena">Arena:</label>
+        <input type="text" id="arena" name="arena" required><br><br>
+    
+        <label for="players">Jugadores:</label>
+        <input type="number" id="players" name="players" required><br><br>  
 
+        <label for="established-year">Año de fundación:</label>
+        <input type="number" id="established-year" name="established_year" required><br><br>
+    
+        <input type="submit" value="send">
+    
+    </form>
 </body>
 </html>

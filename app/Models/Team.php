98f68e5a683bf5ccc8e78a40dd2fd80bd2278fc8
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-04 22:03:45 +0200

    adding a boolean variable  with value false, to omit timestamps field

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     use HasFactory;
-    
+    public $timestamps = false;
     protected $guarded = [];
     //relaccion inversa con model User
     public function user(){

9bb77beafcb596292e07a4426fd23fb7448f73e5
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-04 22:14:26 +0200

    adding buttons and table to teams.index view

--- resources/views/Teams/index.blade.php
+++ resources/views/Teams/index.blade.php
@@ -2,10 +2,17 @@
 @section('title', 'Team')
 @section('content')
 <h1>Todos los equipos</h1>
-<a href="">Registrar equipo</a>
-<a href="">Crear Partido</a>
+<button
+        type="button"
+        class="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+      >
+      <a href="{{route('teams.create')}}">Registrar equipo</a>
 
-@endsection
+      </button>
+<a href="{{route('games.index')}}"><button></button></a>
+
+
+    
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,11 +21,67 @@
     <title>Document</title>
 </head>
 <body>
-   
+   <!-- component -->
+<div class="flex flex-col">
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="overflow-hidden">
+          <table class="min-w-full text-center">
+            <thead class="border-b">
+              <tr>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                  Name
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                  Coach
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                  Stadium
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                    Players
+                  </th>
+                  <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                    Established Year
+                  </th>
+              </tr>
+            </thead>
+            
+            <tbody>
+                @foreach ($teams as $team)
+             
+            
+              <tr class="border-b bg-purple-100 border-purple-200">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                   <a href="{{route('teams.show', $team->id)}}"> {{$team->name}} </a>
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    {{$team->coach}}
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    {{$team->stadium}}
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    {{$team->players}}
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    {{$team->established_year}}
+                </td>
+              </tr>
+          
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
     <!-- Show Teams -->
+ 
     <ul>
-        <li><a href=""></a></li>
+        <li></li>
     </ul>
+   
 </body>
 </html>
 @endsection

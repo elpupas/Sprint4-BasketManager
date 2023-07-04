5de53be7212fffdb2ca18fbdd2ea9bcbfdabfa2e
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-04 22:40:10 +0200

    form to create teams

--- resources/views/Teams/create.blade.php
+++ resources/views/Teams/create.blade.php
@@ -12,23 +12,68 @@
 </head>
 <body>
  
-    <form action="" method="post">
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="name" required><br><br>
+ 
+    <form action="{{route('teams.store')}}" method="post">
+        @csrf
+        <label for="nombre">Nombre:
+            <br>
+            <input type="text" id="nombre" name="name" value="{{old('name')}}" ><br>
+        </label>
+        
+        @error('name')
+        <small>*{{ $message }}</small>
+        <br>
+    @enderror
+    <br>
     
-        <label for="coach">Coach:</label>
-        <input type="text" id="coach" name="coach" required><br><br>
+        <label for="coach">Coach:
+            <br>
+            <input type="text" id="coach" name="coach" value="{{old('coach')}}"  ><br>
+        </label>
+        @error('coach')
+        <small>*{{ $message }}</small>
+        <br>
+    @enderror
+    <br>
     
-        <label for="arena">Arena:</label>
-        <input type="text" id="arena" name="arena" required><br><br>
+        <label for="arena">Stadium:
+            <br>
+            <input type="text" id="stadium" name="stadium" value="{{old('stadium')}}" ><br>
+        </label>
+        
+        @error('stadium')
+        <small>*{{ $message }}</small>
+        <br>
+    @enderror
+    <br>
     
-        <label for="players">Jugadores:</label>
-        <input type="number" id="players" name="players" required><br><br>  
+        <label for="players">Jugadores:
+            <br>
+            <input type="number" id="players" name="players" value="{{old('players')}}"  ><br>
+        </label>
+        @error('players')
+        <small>*{{ $message }}</small>
+        <br>
+    @enderror
+       <br>  
 
-        <label for="established-year">Año de fundación:</label>
-        <input type="number" id="established-year" name="established_year" required><br><br>
-    
-        <input type="submit" value="send">
+        <label for="established_year">Año de fundación:
+            <br>
+            <input type="number" id="established-year" name="established_year" value="{{old('established_year')}}" ><br>
+        </label>
+        @error('established_year')
+        <small>*{{ $message }}</small>
+        <br>
+    @enderror
+       <br>  
+
+       <button
+       type="button"
+       class="border border-green-500 bg-green-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline"
+     >
+     <input type="submit" value="send">
+     </button>
+        
     
     </form>
 </body>

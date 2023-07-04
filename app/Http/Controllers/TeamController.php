e9a1705ea111b790fa2656bc7f8643c4cce404ad
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-04 18:59:17 +0200

    adding user id to Teams table and validation in Team controller update method

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -31,12 +31,32 @@ class TeamController extends Controller
         return view('teams.edit', compact('team'));
     }
     public function store(StoreTeam $request){
+        //vincular id de user al campo user_id
+        $request->merge(['user_id' => $request->user()->id]);
         $team = Team::create($request->all());
-        
-
+      /*  $team = new Team();
+        $team->name = $request->name;
+        $team->coach = $request->coach;
+        $team->arena = $request->arena;
+        $team->players = $request->players;
+        $team->established_year = $request->established_year;
+        */
+     //  $user_id = Auth::id();
+     //  $team->user_id = $user_id;
+      //  $team->save();
         return redirect()->route('teams.show', $team);
     }
     public function update(Request $request, Team $team){
+        $request->validate(
+            [
+                'name' => 'required|min:5',
+                'coach'=>'required|min:5',
+                'stadium'=>'required|min:5',
+                'players'=>'required, max:55',
+                'establihed_year'=>'required',
+
+            ]
+        );
         $team->update($request->all());
 
         return redirect()->route('teams.show', $team);

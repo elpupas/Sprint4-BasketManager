28cd92d98ec32dc87f9674ecc300f0daa7d5aeba
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-05 17:02:05 +0200

    adds button

--- resources/views/Teams/show.blade.php
+++ resources/views/Teams/show.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.plantilla')
 @section('title', 'Show')
 @section('content')
-    <h1>mosttrar</h1>
-    <a href="{{ route('teams.index') }}"><button>Index</button></a>
-    <a href="{{ route('teams.create') }}"><button>Crear Equipo</button></a>
-    <a href="{{ route('games.create', $team->id) }}">Crear Partido</a>
+
+   
 
     <!DOCTYPE html>
     <html lang="en">
@@ -16,38 +14,96 @@
     </head>
 
     <body>
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nombre Equipo</th>
-                        <th>Coach</th>
-                        <th>Stadium</th>
-                        <th>Players</th>
-                        <th>Established year</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{{ $team->name }}</td>
-                        <td>{{ $team->coach }}</td>
-                        <td>{{ $team->stadium }}</td>
-                        <td>{{ $team->players }}</td>
-                        <td>{{$team->established_year}}</td>
-                    </tr>
-                </tbody>
 
-            </table>
+      <x-app-layout>
+        <x-slot name="header">
+          <div class="flex space-x-14  justify-center ">
+            <x-secondary-button
+                type="button"
+                class="border border-indigo-500 bg-indigo-500 text-black rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+            >
+                <a href="{{route('teams.edit', $team)}}">Edit Team</a>
+            </x-secondary-button>
+            <x-secondary-button
+            type="button"
+            class="border border-indigo-500 bg-indigo-500 text-black rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+        >
+            <a href="{{route('games.create', $team)}}">Create Game</a>
+        </x-secondary-button>
+            
+            <div>
+                <form action="{{ route('teams.destroy', $team) }}" method="post">
+                    @csrf
+                    @method('delete')
+                    <x-secondary-button
+                        type="button"
+                        class="border border-indigo-500 bg-indigo-500 text-black rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+                    >
+                        Delete Team
+                    </x-secondary-button>
+                </form>
+            </div>
         </div>
-          <div>
-        <a href="{{ route('teams.edit', $team->id) }}"><button>Editar Team</button></a>
-
-        <form action="{{ route('teams.destroy', $team) }}" method="post">
-            @csrf
-            @method('delete')
-            <button>Eliminar</button>
-        </form>
-      </div>
+        
+      </x-slot>
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+          <div class="overflow-hidden">
+            <table class="min-w-full text-center">
+              <thead class="border-b">
+                <tr>
+                  <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                    Name
+                  </th>
+                  <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                    Coach
+                  </th>
+                  <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                    Stadium
+                  </th>
+                  <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                      Players
+                    </th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                      Established Year
+                    </th>
+                </tr>
+              </thead>
+              
+              <tbody>
+               
+               
+              
+                <tr class="border-b bg-purple-100 border-pink-200">
+                  <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                   {{$team->name}}
+                  </td>
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {{$team->coach}}
+                  </td>
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {{$team->stadium}}
+                  </td>
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {{$team->players}}
+                  </td>
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {{$team->established_year}}
+                  </td>
+                </tr>
+            
+               
+              </tbody>
+            </table>
+          </div>
+        </div>
+        </div>
+      
+      
+      
+      </x-app-layout>
+     
+  
     </body>
 
     </html>

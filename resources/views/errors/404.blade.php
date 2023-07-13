a374bb2b043283678095a4ea3c2a3a0ad8048042
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-13 12:16:17 +0200

    install Error views

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+@extends('errors::layout')
+
+
+@section('title', __('Not Found'))
+@section('code', '404')
+@section('message', __('Not Found '))
+
+
+
+
